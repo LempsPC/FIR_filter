5e3d3116a3e8c81f2f6d413766ddc9cc7be986a3
LempsPC <lemps5245@gmail.com>
2018-05-19 17:47:35 +0300

    added files

---
+++ FIR_filter3/FIR_beh.vhd
@@ -0,0 +1,56 @@
+---------------------------------------------------------------------
+--  9-tab FIR filter -- behavioral model
+---------------------------------------------------------------------
+--  Data word - 16 bits: sign, 5 bits left & 10 bits right of point
+--    "sddddd.ffffffffff"  ["d"~integer, "f"~fraction]
+--    1.0 == "0000010000000000", 0.125 == "0000000010000000", etc.
+---------------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+
+entity  fir_filter is
+  port ( data_in: in signed (15 downto 0);
+         data_out: out signed (15 downto 0);
+         sample, clk: in bit );
+end fir_filter;
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+
+architecture  behave  of  fir_filter  is
+  type array_type is array (1 to 9) of signed (15 downto 0);
+  -- (0.25, 0.25, -0.75, 1.25, 1.25, 1.25, -0.75, 0.25, 0.25)
+  constant coeffs: array_type := (
+    "0000000100000000", "1111111100000000", "0000010010000000",
+    "0000010100000000", "0000010100000000", "0000010100000000",
+    "0000010010000000", "1111111100000000", "0000000100000000" );
+begin
+
+  -- FIR filter as a single process
+  process
+    variable delayed: array_type;
+    variable sum: signed (15 downto 0);
+    variable tmp: signed (31 downto 0);
+  begin
+    -- Waiting for a new sample
+    wait on clk until clk='1' and sample='1';
+
+    -- Outputting results of the previous sample
+    data_out <= sum;
+
+    -- Shift and latch
+    delayed (1 to 8) := delayed (2 to 9);
+    delayed (9) := data_in;
+
+    -- Calculate
+    sum := (others=>'0');
+    for i in array_type'range loop
+      tmp := coeffs(i) * delayed(i);
+      sum := sum + tmp(25 downto 10);
+    end loop;
+
+  end process;
+
+end behave;
---
+++ FIR_filter3/FIR_tester1.vhd
@@ -0,0 +1,92 @@
+----------------------------------------------------------------------------------
+-- Company:
+-- Engineer:
+--
+-- Create Date: 03/02/2018 12:00:01 PM
+-- Design Name:
+-- Module Name: FIR_tester1 - Behavioral
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+----------------------------------------------------------------------------------
+ 
+ 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_arith.all;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+ 
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+ 
+entity FIR_tester1 is
+--  Port ( );
+end FIR_tester1;
+ 
+architecture Behavioral of FIR_tester1 is
+ 
+component  fir_filter_syn is
+  port ( data_in: in signed (15 downto 0);
+         data_out: out signed (15 downto 0);
+         sample, clk: in bit );
+end component fir_filter_syn;
+ 
+signal data_in, data_out: signed (15 downto 0);
+signal sample, clk: bit;
+ 
+procedure cycle (signal data_in : out signed(15 downto 0);
+                 signal sample :out bit;
+                 signal clk : out bit ) is
+begin
+    clk <= '1';
+    sample <= '1';
+    wait for 10ns;
+    
+    clk <= '0';
+    
+    wait for 10ns;
+    sample <= '0';
+    for i in 0 to 2 loop
+        clk <= '1';
+        wait for 10ns;
+        clk <= '0';
+        wait for 10ns;
+    end loop;
+end cycle;
+ 
+ 
+begin
+ 
+UUT: fir_filter_syn
+Port map (data_in => data_in,
+          data_out => data_out,
+          sample => sample,
+          clk => clk);
+ 
+stimuli: process
+begin
+   
+    for i in 0 to 10 loop
+        data_in <= "0000000000000000";
+        cycle(data_in, sample, clk);
+    end loop;
+   
+    data_in <= "0000010000000000";
+    cycle(data_in, sample, clk);
+   
+end process;
+ 
+end Behavioral;
